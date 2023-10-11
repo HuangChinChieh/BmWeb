c0a08cdef14c92bc286553c4f649dd2ce6ccf557
meteor950187 <meteor950187@gmail.com>
2023-10-11 19:28:37 +0800

    1

--- App_Code/EWinWeb.cs
+++ App_Code/EWinWeb.cs
@@ -19,6 +19,7 @@ public static class EWinWeb {
     public static string CompanyCode = System.Configuration.ConfigurationManager.AppSettings["CompanyCode"];
     public static string WebUrl = System.Configuration.ConfigurationManager.AppSettings["WebUrl"];
     public static string EWinUrl = System.Configuration.ConfigurationManager.AppSettings["EWinUrl"];
+    public static string EWinAPIUrl = System.Configuration.ConfigurationManager.AppSettings["EWinAPIUrl"];
     public static string ImageUrl = System.Configuration.ConfigurationManager.AppSettings["ImageUrl"];
     public static string EWinAgentUrl = System.Configuration.ConfigurationManager.AppSettings["EWinAgentUrl"];
     public static string MainCurrencyType = System.Configuration.ConfigurationManager.AppSettings["MainCurrencyType"];
